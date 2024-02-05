2301d3913cdf7e66d5c4bd09c14b86c8da101381
FMystek <filip.mystek@gmail.com>
2024-02-05 09:58:51 +0100

    Dodanie prostego frontu, opcji wczytania pliku

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# This is a sample Python script.
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+
+# Folder do przechowywania wczytanych zdjęć
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/')
+def upload_file():
+    return render_template('upload.html')
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/predict', methods=['POST'])
+def predict():
+
+        return 'Predykcja marki samochodu: ...'  # Zwróć wynik predykcji
+
+
 if __name__ == '__main__':
-    #etst
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
